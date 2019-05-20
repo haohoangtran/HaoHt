451dd578cf5c3adb2a78b9373307d4542e0bbb93
haohoangtran <tranhaockpt@gmail.com>
2019-05-20 17:48:56 +0700

    fix

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const bodyParser = require('body-parser')
-const exphbs  = require('express-handlebars');
+const exphbs = require('express-handlebars');
 const app = express()
 app.engine('handlebars', exphbs());
 app.set('view engine', 'handlebars');
@@ -13,22 +13,29 @@ app.listen(process.env.port || process.env.PORT || 3000, () => {
     console.log("Ok")
 });
 
-function capitalizeFirstLetter(string) {
-    return string.charAt(0).toUpperCase() + string.slice(1);
+function humanize(str) {
+    str = str.toLowerCase();
+    var frags = str.split(' ');
+
+    for (i = 0; i < frags.length; i++) {
+        frags[i] = frags[i].charAt(0).toUpperCase() + frags[i].slice(1);
+    }
+    return frags.join(' ');
 }
 
+
 app.get("/:name", (req, res) => {
-    let name = req.params.name||"You";
-    name = name.toLowerCase();
-    name = capitalizeFirstLetter(name);
-    res.render("main",{name,layout:false,imagebg:`images/countdown-${getRandomArbitrary()}-1600x900.jpg`})
+    let name = req.params.name || "You";
+    name = humanize(name)
+    res.render("main", {name, layout: false, imagebg: `images/countdown-${getRandomArbitrary()}-1600x900.jpg`})
 })
 app.get("/", (req, res) => {
     res.render("main", {name: "There", layout: false, imagebg: `images/countdown-${getRandomArbitrary()}-1600x900.jpg`})
 })
+
 function getRandomArbitrary() {
-    let num= Math.floor(Math.random()*100)%3;
+    let num = Math.floor(Math.random() * 100) % 3;
     console.log(num)
-    return num+5;
+    return num + 5;
 }
 
